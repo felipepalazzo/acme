d282f42a47a97211db60312026a0ea38f514c9ee
Felipe Palazzo <palazzofelipe@gmail.com>
2016-10-15 11:51:28 -0300

    Finish places autocomplete

---
+++ app/constants.js
@@ -0,0 +1,6 @@
+export const SP_COORDINATES = {
+  north: -23.356604,
+  south: -24.008221,
+  east: -46.365084,
+  west: -46.825514,
+};
--- app/form/form.component.js
+++ app/form/form.component.js
@@ -1,33 +1,25 @@
 /* globals google */
-import {Marionette} from '../../vendor/vendor';
+import { Marionette } from '../../vendor/vendor';
 import template from './form.template.jst.ejs';
+import { SP_COORDINATES } from '../constants';
 
 export default Marionette.View.extend({
   tagName: 'form',
   template: template,
-  ui: {
-    route: '[data-ui="route"]',
-  },
   events: {
     'submit': 'onSubmit',
   },
-  COORDINATES: {
-    // The results are biased towards, but not restricted to,
-    // places contained within these bounds (São Paulo)
-    north: -23.356604,
-    south: -24.008221,
-    east: -46.365084,
-    west: -46.825514,
-  },
-  componentForm: {
-    street_number: 'short_name',
-    route: 'long_name',
-    postal_code: 'short_name',
-  },
+  addressInputs: [
+    'street_number',
+    'route',
+    'postal_code',
+    'sublocality_level_1',
+  ],
   onRender() {
+    var coordinates = SP_COORDINATES;
     var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(this.COORDINATES.south, this.COORDINATES.west),
-      new google.maps.LatLng(this.COORDINATES.north, this.COORDINATES.east)
+      new google.maps.LatLng(coordinates.south, coordinates.west),
+      new google.maps.LatLng(coordinates.north, coordinates.east)
     );
     this.autocomplete = new google.maps.places.Autocomplete(
       this.$('#route')[0], { types: ['geocode'], bounds: defaultBounds });
@@ -36,12 +28,15 @@ export default Marionette.View.extend({
   onSubmit: function(evt) {
     evt.preventDefault();
   },
+  setInputValue(inputUi, value) {
+    this.ui[inputUi].val()
+  },
   fillInAddress() {
     var place = this.autocomplete.getPlace();
     for (var i = 0; i < place.address_components.length; i++) {
       var addressType = place.address_components[i].types[0];
-      if (this.componentForm[addressType]) {
-        var val = place.address_components[i][this.componentForm[addressType]];
+      if (this.addressInputs.includes(addressType)) {
+        var val = place.address_components[i].short_name;
         this.$('#' + addressType).val(val);
       }
     }
